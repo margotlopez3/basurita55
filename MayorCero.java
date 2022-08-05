f48bbeb60d3269fb8b5094fc1afedab4c6141f5e
margotlopez3 <75349855+margotlopez3@users.noreply.github.com>
2022-08-05 09:11:40 -0500

    Add files via upload

---
+++ MayorCero.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * Programas  creados  por  Diana Margot López Herrera
+ * para el curso de  Tecnicas de programación
+ * Pueden ser usados  por los estudiantes con fines académicos
+ */
+
+package Utilidades;
+
+/**
+ *
+ * @author estudiante
+ */
+public class MayorCero extends Exception {
+    public MayorCero(String mensaje, int numero){
+        if (numero<0){
+            System.out.println("mensaje");
+        }
+    }
+            
+}
---
+++ Viernes13Exception.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * Programas  creados  por  Diana Margot López Herrera
+ * para el curso de  Tecnicas de programación
+ * Pueden ser usados  por los estudiantes con fines académicos
+ */
+
+package Utilidades;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Viernes13Exception extends Exception {
+
+    /**
+     * Creates a new instance of <code>Viernes13Exception</code> without detail
+     * message.
+     */
+    public Viernes13Exception() {
+    }
+
+    /**
+     * Constructs an instance of <code>Viernes13Exception</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public Viernes13Exception(String msg) {
+        super(msg);
+    }
+}
